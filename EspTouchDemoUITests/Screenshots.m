20c808bd6caa1309df3efe2415f99123ba0823b8
Florian BUREL <florian.burel@mac.com>
2018-10-22 11:13:02 +0200

    fstlane int + screenshot

--- EspTouchDemoUITests/Screenshots.m
+++ EspTouchDemoUITests/Screenshots.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "EspTouchDemoUITests-Swift.h"
 
 @interface EspTouchDemoUITests : XCTestCase
 
@@ -21,18 +22,29 @@
     self.continueAfterFailure = NO;
 
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    XCUIApplication * app = [[XCUIApplication alloc] init];
+    
+     // Setup fastlane snapshot
+    [Snapshot setupSnapshot:app];
+    
+    // warn the app that she should run in screenshot mode
+    app.launchArguments = [app.launchArguments arrayByAddingObject:@"Screenshots"];
+    
+    // Finally launches the app
+    [app launch];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
+- (void) test_01 {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // take capture of the main screen
+    [Snapshot snapshot:@"MainScreen" timeWaitingForIdle:2];
+    
 }
 
 @end
